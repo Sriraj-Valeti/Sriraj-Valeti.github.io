5a1c0442995621cef00f84d3e4f166c2562f8eb3
Sriraj-Valeti <me19b055@smail.iitm.ac.in>
2023-11-04 21:58:58 +0530

    first commit

---
+++ src/components/card/card.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './card.css';
+
+
+
+function Card({id='no',title='',tag='',status='',priority=''}){
+    return(
+        <div className="Card-Container">
+            <div className="Card-Id">{id}</div>
+            <div className="Card-Heading">{title}</div>
+            <div className="Card-Tag"><span className="Card-Priority">{priority}</span><span className="Card-Feature">{tag[0]}</span></div>
+        </div>
+    )
+}
+
+
+export default Card;
